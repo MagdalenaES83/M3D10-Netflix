6733bb2bea1c3b0f9e0e3763f37d5cd87999c6af
Magdalena <84572667+MagdalenaES83@users.noreply.github.com>
2021-07-02 16:48:50 +0200

    Add files via upload
    work in progress

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = () =>{
+//window.onload = () =>{
    //alert("hello world")
 
 // token : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNmQyM2IzNTgxNzAwMTVjMjI3NTEiLCJpYXQiOjE2MjUyMTE5MzksImV4cCI6MTYyNjQyMTUzOX0.sqR7yOhKlqGFnaw6vg1Rf6InreVSCgTBHxpudMbcFAo
@@ -14,14 +14,16 @@ POST https://striveschool-api.herokuapp.com/api/account/login
     "username": "testusername",
     "password":"pass"
 }   
-*/
+*//*
 const url = "https://striveschool-api.herokuapp.com/api/movies/"
 const headers = new Headers ({
     "Content-Type":  "application/json", 
     "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNmQyM2IzNTgxNzAwMTVjMjI3NTEiLCJpYXQiOjE2MjUyMTE5MzksImV4cCI6MTYyNjQyMTUzOX0.sqR7yOhKlqGFnaw6vg1Rf6InreVSCgTBHxpudMbcFAo"
 })
 
+
 const movies = (e) => {
+    console.log(123)
     e.preventDefault()
     try {
         let dataFromForm = {
@@ -48,5 +50,33 @@ const movies = (e) => {
     }
 }
 
+document.getElementById("getText").addEventListener("click", movies()); */
 
-}
+
+//document.getElementById("getMovie").addEventListener("click", getMovie);
+
+
+
+function addMovie(e){
+    e.preventDeFault();
+
+    let dataFromForm = {
+    title :  document.getElementById("title").value,
+    description : document.getElementById("description").value,
+    category :  document.getElementById("category").value,
+    poster :  document.getElementById("poster").value,
+    }
+    console.log(dataFromForm)
+
+    fetch("movies.json", { 
+        method: "POST",
+        body: JSON.stringify(dataFromForm),
+        headers: {"Accept" : "application/json text/plain, */*" ,  "Content-Type": "application/json",}          
+        },    
+          )
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+     } 
+
+document.getElementById("addMovie").addEventListener("submit",addMovie)
